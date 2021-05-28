9ff2da8460d70e591f7c9e1e30644b0fbc26f392
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-05-28 18:35:10 +0430

    end of v-bind

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,17 @@
 <!--conditional class array and obj -->
 <h3 v-bind:class="[ispromoted && 'success', disabled ? 'align' :'' ]">this condition with array</h3>
 <h2 v-bind:class = "{  'success' : ispromoted}">hi honey </h2>
+
+
+<!-- inline style -->
+<h3 v-bind:style="{
+	color  : highlight, 
+	'font-size' : fontHigh +'px',
+	'background-color' :back 
+}">Font and  color inline </h3>
+<!-- inline with obj-->
+<p v-bind:style="objStyle1">kia with obj style ---</p>
+<div v-bind:style="[objStyle1,objStyle2]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, voluptas.</div>
 </template>
 
 <script>
@@ -27,6 +38,18 @@ return {
 	disabled :  true,
 	ispromoted : true , 
 	status  : "success" , 
+	highlight : 'crimson',
+	back :  'blue',
+	fontHigh : 30 ,
+	objStyle1 :{
+		font : '10px',
+		color : 'crimson',
+		backgroundColor :  'yellow'
+	},
+	objStyle2 :{
+		fontSize : '40px',
+		
+	}
 }
 
  },
