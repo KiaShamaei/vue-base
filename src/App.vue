9539fd5aa2ff3bc2c4002fe7f66070d48013b48d
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-05-28 16:54:10 +0430

    vue v-bind and conditional class

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
 <h1> kia is bala ! {{name}}</h1>
 <div v-text="lname"></div> 
 <div v-html="camel"></div>
+<div v-bind:id="headId"> this is a tag with heading id </div>
+<button v-bind:disabled="disabled" v-on="handleChange">bind value </button>
+<p v-bind:class="status"> this is p with dynamic class</p>
+
+<!--this condition class  -->
+<h2 v-bind:class = "ispromoted &&  'success'">this is condition </h2>
+<!--conditional class array and obj -->
+<h3 v-bind:class="[ispromoted && 'success', disabled ? 'align' :'' ]">this condition with array</h3>
+<h2 v-bind:class = "{  'success' : ispromoted}">hi honey </h2>
 </template>
 
 <script>
@@ -13,9 +22,21 @@ export default {
 return {
 	name :  "shamae" ,
 	lname : "vishvas",
-	camel : "<b>kia is good learner</b>"
+	camel : "<b>kia is good learner</b>",
+	headId : "heading",
+	disabled :  true,
+	ispromoted : true , 
+	status  : "success" , 
 }
- }
+
+ },
+//  methods: {
+// 	 handleChange :function(){
+// 		 this.status = "failed"
+// 	 }
+
+//  }
+
 
  }
 </script>
@@ -28,5 +49,15 @@ return {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.success {
+	color : greenyellow ;
+}
+.failed {
+	color : crimson;
+}
+.align{
+	font-size: 400;
+	background-color: coral ;
 }
 </style>
