0361f8f2ee4939523cc1724b2949d9f5b5d55b62
kiarash.shamaii <kiarash.shamaii@gmail.com>
2021-05-30 20:46:17 +0430

    m first vue

---
+++ src/App.-rendering-list .vue
@@ -0,0 +1,50 @@
+<template>
+
+<h3 v-if="isShow">this test for condition </h3>
+<h3 v-if="num === 6">this test for condition </h3>
+
+<h3 v-else-if="num < 5">this test for condition </h3>
+<h3 v-if="num === 5">this test for {{num}} </h3>
+<!-- if we want to cancel more than one elements-->
+<!-- <template v-if="isShow">
+	<p> Lorem ipsum dolor sit amet.</p>
+	<p>Lorem ipsum dolor sit amet.</p>
+	<p>Lorem ipsum dolor sit amet.</p>	
+</template> -->
+<ul >
+	<li v-for="name in names" :key="name">{{name}}</li>
+	<li v-for="(name,index) in names" :key="index">{{name}}</li>
+</ul>
+<ul>
+	<li v-for="(name,index) in fullnames" :key="index">{{name.fname}} - --- {{name.lname}}</li>
+</ul>
+
+
+<h2>kia is balaei pesar</h2>
+  
+</template>
+
+<script>
+export default {
+	name : "App" ,
+	data (){
+		return {
+			isShow : false , 
+			num  : 5 , 
+			names : ["kia", "mina", "sina"],
+			fullnames : [
+				{fname : "kia" , lname : "shamaei"},
+				{fname : "pari" , lname : "zhd"},
+				{fname : "yasi" , lname : "ghami"}
+			]
+		}
+
+	}
+
+}
+</script>
+
+<style>
+
+
+</style>
